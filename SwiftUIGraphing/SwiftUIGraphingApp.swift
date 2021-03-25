dd944c8f1872071bc7bbf7eb81c6edde1ce36bbf
Brigette Valdez <valdez.brigette@gmail.com>
2021-03-25 12:06:55 -0500

    Initial Commit

---
+++ SwiftUIGraphing/SwiftUIGraphingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGraphingApp.swift
+//  SwiftUIGraphing
+//
+//  Created by Brigette Valdez on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGraphingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
